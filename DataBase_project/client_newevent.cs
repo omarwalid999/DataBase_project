05b4d7fe88ba57645809a837ba67f02ad6b8eeb4
noortantawy <noortantawy27@gmail.com>
2024-12-26 22:23:39 +0200

    heyy

--- DataBase_project/client_newevent.cs
+++ DataBase_project/client_newevent.cs
@@ -17,6 +17,8 @@ namespace DataBase_project
         public client_newevent(int id)
         {
             InitializeComponent();
+            hotelbox1.Hide();
+            beachbox1.Hide();
             this.id2 = id;
             Controller cobj = new Controller();
             DataTable dt = cobj.ShowTypes();
@@ -27,7 +29,13 @@ namespace DataBase_project
 
         private void typesofevents_SelectedIndexChanged(object sender, EventArgs e)
         {
-            //
+            if(typesofevents.SelectedIndex == 1) //wedding
+            {
+                hotelbox1.Show();
+                beachbox1.Show();
+            } else if (){
+
+            }
         }
 
         private void client_newevent_Load(object sender, EventArgs e)
